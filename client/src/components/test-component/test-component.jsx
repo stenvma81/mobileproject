29b920649c9817a9f43a26c52332e3b985eebc42
Matti Stenvall <matti.stenvall@gmail.com>
2022-11-17 14:49:51 +0200

    unused code removed

--- client/src/components/test-component/test-component.jsx
+++ client/src/components/test-component/test-component.jsx
@@ -1,37 +1,8 @@
-import React, { Alert } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { SendMessage } from '../messages/SendMessage'
 import { PostForm } from '../posts/PostForm';
-
-const alert = () => {
-  return(
-    <Alert severity="info">This is an info alert — check it out!</Alert>
-  )
-}
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React to be a king
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 
 export function TestComponent() {
   return (
