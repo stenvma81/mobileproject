cd19fb3b092bf4085827ffb51a50ccb5ea842854
Tatu <tatu.d.ihaksi@gmail.com>
2022-11-22 15:18:12 +0200

    created time formatted by using moment

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TestComponent, Preferences, Dashboard, Login } from './components';
 import useToken from './hooks/LoginHooks';
+import { MainView } from './components/main/MainView';
 
 function App() {
   const { token, setToken } = useToken();
 
-  if(!token) {
-    return <Login setToken={setToken} />
+  if (!token) {
+    return <Login setToken={setToken} />;
   }
 
   return (
     <div className="wrapper">
       <BrowserRouter>
         <Routes>
-        <Route path="/" element= {<TestComponent/>}/>
-          <Route path="/dashboard" element= {<Dashboard/>}/>
-          <Route path="/preferences" element= {<Preferences/>}/>
+          {/* <Route path="/" element= {<TestComponent/>}/> */}
+          <Route path="/" element={<MainView />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
         </Routes>
       </BrowserRouter>
     </div>
-
   );
-  
 }
-
-
 
 export default App;
