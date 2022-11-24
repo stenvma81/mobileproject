783692580053d2be042ae7544319363615fa3296
maijuhimberg <maiju.himberg@metropolia.fi>
2022-11-24 18:38:39 +0200

    main view with buttons

--- client/src/components/main/MainView.jsx
+++ client/src/components/main/MainView.jsx
@@ -1,14 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import React from 'react';
 import { PostForm } from '../posts/PostForm';
 import { PostList } from '../posts/PostList';
 import './styles.css';
 
 export function MainView() {
   useEffect(() => {}, []);
+  const [FormIsOpen, setIsOpen] = useState(false);
+  const handleOpenForm = (event) => {
+    event.preventDefault();
+    if (event.target === event.currentTarget && FormIsOpen) {
+      setIsOpen(!FormIsOpen);
+    }
+    !FormIsOpen && setIsOpen(!FormIsOpen);
+  };
+  
 
   return (
     <div id="container">
-      <PostForm />
+      <div className='buttons_container'>
+        <div className='button1'>
+          <button onClick={handleOpenForm}>Service Advice</button>
+        </div>
+        <div className='button2'>
+          <button onClick={handleOpenForm}>Feedback</button>
+      </div>
+          <div className='button3'>
+      <button onClick={handleOpenForm}>Development Proposal</button>
+      </div>
+      <div className='button4'>
+      <button onClick={handleOpenForm}>Question</button>
+      </div>
+      </div>
+      {FormIsOpen && (
+        <PostForm/>)
+      }
+      <h3>Previous posts:</h3>
       <PostList />
     </div>
   );
