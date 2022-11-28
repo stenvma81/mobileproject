841a09b67877837af29671898aab76509d9a6a16
maijuhimberg <maiju.himberg@metropolia.fi>
2022-11-28 15:23:16 +0200

    Admin header and responsivnes to admin main view

---
+++ client/src/components/admin/admin-header/admin-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from './nokia_header_desktop.jpg';
+import './admin-header.css'
+
+export function AdminHeader(){
+    return(
+        <div className="admin_image_header" style={{backgroundImage: 'url('+ Background +')'
+    }}>
+            <h1 id="bigger">Karamalmi Campus</h1>
+            <h2>Kiinteistöt</h2>
+    </div>
+    )
+}
