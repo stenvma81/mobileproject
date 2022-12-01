e20524c7c908fc944ecd4f6a09e91fa4a5e4d092
maijuhimberg <maiju.himberg@metropolia.fi>
2022-12-01 11:52:40 +0200

    nothing to push

--- client/src/components/admin/admin-header/admin-header.js
+++ client/src/components/admin/admin-header/admin-header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Background from './nokia_header_desktop.jpg';
 import './admin-header.css'
 
+
 export function AdminHeader(){
     return(
         <div className="admin_image_header" style={{backgroundImage: 'url('+ Background +')'
