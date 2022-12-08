307ea728b1557cab17777d44400a3f9570801f5e
Tatu <tatu.d.ihaksi@gmail.com>
2022-12-08 13:23:02 +0200

    hide modify messages button when modifying

--- client/src/components/messages/ModifieMessageField.jsx
+++ client/src/components/messages/ModifieMessageField.jsx
@@ -22,7 +22,7 @@ export function ModifieMessageField({ message, setIsModifying }) {
   };
 
   return (
-    <div>
+    <div className="column">
       <textarea
         className="message-textfield"
         rows="4"
