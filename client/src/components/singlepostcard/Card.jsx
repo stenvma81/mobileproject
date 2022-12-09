95b3e520c524374806ab717ca8902a7579ed1ee3
Tatu <tatu.d.ihaksi@gmail.com>
2022-12-09 14:11:13 +0200

    something

--- client/src/components/singlepostcard/Card.jsx
+++ client/src/components/singlepostcard/Card.jsx
@@ -1,20 +1,20 @@
-import React, { useContext, useState } from "react";
-import Moment from "react-moment";
-import "moment-timezone";
-import { MessageList } from "../messages/MessageList";
-import { SendMessage } from "../messages/SendMessage";
-import PropTypes from "prop-types";
-import logo from "../header/images/nokia.jpg";
-import MapModal from "../map-modal/MapModal";
-import {ImageModal} from "../image-modal/ImageModal";
-import "./smallCard.css";
-import { ModifyPostState } from "../admin/ModifyPostState";
-import { MdClose, MdCreate, MdSocialDistance } from "react-icons/md";
-import { FaPen, FaStarOfLife } from "react-icons/fa";
-import { NewMessagesCount } from "./NewMessagesCount";
-import { MainContext } from "../../context/MainContext";
-import { userRoles } from "../../utils/variables";
-import { ModifyPost } from "../posts/ModifyPost";
+import React, { useContext, useState } from 'react';
+import Moment from 'react-moment';
+import 'moment-timezone';
+import { MessageList } from '../messages/MessageList';
+import { SendMessage } from '../messages/SendMessage';
+import PropTypes from 'prop-types';
+import logo from '../header/images/nokia.jpg';
+import MapModal from '../map-modal/MapModal';
+import { ImageModal } from '../image-modal/ImageModal';
+import './smallCard.css';
+import { ModifyPostState } from '../admin/ModifyPostState';
+import { MdClose, MdCreate, MdSocialDistance } from 'react-icons/md';
+import { FaPen, FaStarOfLife } from 'react-icons/fa';
+import { NewMessagesCount } from './NewMessagesCount';
+import { MainContext } from '../../context/MainContext';
+import { userRoles, imageUrl } from '../../utils/variables';
+import { ModifyPost } from '../posts/ModifyPost';
 
 export default function Card({ post }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,17 +28,17 @@ export default function Card({ post }) {
     const marker = JSON.parse(post.areamarker);
     setMarkers([...markers.splice(0, marker), marker]);
     setShowModal(!showModal);
-  }
+  };
 
   const openImage = () => {
     setShowImageModal(!showImageModal);
-  }
+  };
 
   const TypeDot = () => {
-    let color = "rgba(6,177,169,1)";
-    post.typeid === 1 && (color = "rgba(135,110,255,1)");
-    post.typeid === 3 && (color = "rgba(246,13,13,1)");
-    return <div className='dot' style={{ backgroundColor: color }} />;
+    let color = 'rgba(6,177,169,1)';
+    post.typeid === 1 && (color = 'rgba(135,110,255,1)');
+    post.typeid === 3 && (color = 'rgba(246,13,13,1)');
+    return <div className="dot" style={{ backgroundColor: color }} />;
   };
 
   const handleParentClick = (event) => {
@@ -54,46 +54,46 @@ export default function Card({ post }) {
   }
 
   return (
-    <div className='card' onClick={!isOpen ? handleParentClick : undefined}>
-      <div className='form-title'>
+    <div className="card" onClick={!isOpen ? handleParentClick : undefined}>
+      <div className="form-title">
         <NewMessagesCount post={post} />
         {isOpen && (
-          <div className='close-modify'>
+          <div className="close-modify">
             <MdCreate
-              className='close-x'
+              className="close-x"
               onClick={() => setIsModifying(true)}
             />
-            <MdClose className='close-x' onClick={() => setIsOpen(false)} />
+            <MdClose className="close-x" onClick={() => setIsOpen(false)} />
           </div>
         )}
       </div>
-      <div className='post-state'>
-        <div className='column'>
-          <div className='post-type'>
+      <div className="post-state">
+        <div className="column">
+          <div className="post-type">
             <div>
-              <TypeDot className='dot-border' />
+              <TypeDot className="dot-border" />
               {post.type}
             </div>
-            <div style={{'padding-top': '5px'}}>{post.state}</div>
+            <div style={{ 'padding-top': '5px' }}>{post.state}</div>
             {/* <NewMessagesCount post={post} /> */}
           </div>
         </div>
 
-        <div className='column'>
-          <Moment date={post.created_date} format='DD.MM.YYYY HH:mm' />
+        <div className="column">
+          <Moment date={post.created_date} format="DD.MM.YYYY HH:mm" />
           {post.closed_date !== null && (
-            <Moment date={post.closed_date} format='DD.MM.YYYY HH:mm' />
+            <Moment date={post.closed_date} format="DD.MM.YYYY HH:mm" />
           )}
         </div>
       </div>
-      <div className='post-text'>
-        <div className='card-title'>{post.title}</div>
-        {isOpen && <div className='place'>{`${post.description}`}</div>}
+      <div className="post-text">
+        <div className="card-title">{post.title}</div>
+        {isOpen && <div className="place">{`${post.description}`}</div>}
       </div>
       {isOpen && (
         <div>
-          <div className='card-text'>{`Location: ${post.location}`}</div>
-          <div className='post-buttons'>
+          <div className="card-text">{`Location: ${post.location}`}</div>
+          <div className="post-buttons">
             <div>
               <button onClick={openModal}>Show location</button>
             </div>
